3f4708f1c50fd76a6749e2f227dad5c846e37086
Anton K <drvirus15@gmail.com>
2026-09-14 23:34:24 +0300

    3.1 utility types(all done)

--- src/myMap.ts
+++ src/myMap.ts
@@ -26,4 +26,54 @@ const studentStatus: MapObjectParams<boolean, number> = {
     transformer: isPassed,
 };
 
-console.log(mapObject(studentStatus));
+// console.log(mapObject(studentStatus));
+// utility types practics
+//1.
+type T = {
+    id: string,
+    name: string,
+    age: number,
+    student: boolean,
+}
+
+function f1(param: Partial<T>) {
+}
+
+function f1Appender(param: Partial<T>): Required<T> {
+    return {} as Required<T>;
+}
+
+//2.
+
+function hardF1(param: Omit<T, 'id'>) {
+
+}
+
+function hardF1Appender(param: Omit<T, 'id'>): Required<T> {
+    return {} as Required<T>;
+}
+
+//3
+
+class Rectangle {
+    w!: number;
+    h!: number;
+}
+class Circle {
+    radius!: number;
+}
+
+// Зробіть норм сигнатуру тут.
+// НІ, Rectangle | Circle це не варіант, треба зробити універсальну функцію
+
+function наштампувати<T extends object>(SOMECLASS: { new(): T }, count: number): T[] {
+    let a: T[] = [];
+    for (let i = 0; i < count; i++)
+        a.push(new SOMECLASS());
+
+    return a;
+}
+
+let a: Rectangle[] = наштампувати(Rectangle, 10);
+let b: Circle[] = наштампувати(Circle, 20)
+console.log(a, b);
